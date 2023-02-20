c58a350627b4ba82d250ef7ea4332579c3397309
MrWhip <gleb.kovalev.02@mail.ru>
2023-02-21 02:27:37 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,11 +38,7 @@ export default {
     // https://go.nuxtjs.dev/axios
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
+  target: 'static',
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
